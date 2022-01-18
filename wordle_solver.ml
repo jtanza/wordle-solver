e9a763f1db582c6584046b76ea3bcc26454953c3
jtanza <johntanza@gmail.com>
2022-01-18 17:06:48 -0500

    adds candidate construction

--- wordle_solver.ml
+++ wordle_solver.ml
@@ -9,15 +9,33 @@ let build_word_list file =
     |> List.filter_map (fun l -> match l with
       | ((a::b::_)) when String.length a = 5 && int_of_string b > 200_000 -> Some((a, int_of_string b))
       | _ -> None)
-    |> List.sort (fun a b -> compare (Tuple2.second a) (Tuple2.second b))
+    |> List.sort (fun a b -> compare (snd a) (snd b))
 
 let build_word_map words = List.fold (fun m t -> StringMap.add (fst t) (snd t) m) StringMap.empty words
 
-let first_guess words = words |> List.take 1_000 |> List.shuffle |> List.first |> Tuple2.first
+let first_guess words = words |> List.take 1_000 |> List.shuffle |> List.first |> fst
 
+type color = Green | Yellow | Gray
 
 module CharMap = Map.Make(Char)
 module CharSet = Set.Make(Char)
+module IntSet = Set.Make(Int)
+
+let build_candidates m =
+  let keepers = CharMap.filterv (fun v -> let color = fst v in (color = Green || color = Yellow)) m in
+    let permutations = CharMap.keys keepers
+      |> Enum.merge (>) @@ Enum.repeat ~times:5 '.'
+      |> Enum.take 5
+      |> List.of_enum
+      |> LazyList.permutations
+      |> LazyList.unique in
+
+  LazyList.filter (fun l -> CharMap.for_all (fun k v -> match List.index_of k l with
+      | None -> true
+      | Some(i) -> match v with
+        | (color, indexes) -> match color with
+          | Green -> IntSet.mem i indexes
+          | Yellow -> not @@ IntSet.mem i indexes) keepers) permutations
 
 type trie = Node of string * trie CharMap.t
 
@@ -44,4 +62,5 @@ let wildcard_search query exclude trie =
       | _, Node(word, _) -> Dllist.add acc word in
     search (String.explode query) trie;
     Dllist.to_list acc
+
 
